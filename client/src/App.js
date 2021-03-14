3555c6036166d8b1ea2632de0fd401b254f51aed
Dmitriy <dmitriy.tyutyunik@gmail.com>
2021-03-14 16:02:59 -0400

    financial investments file has been added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import investments from './financials';
+
+function App() {
+  return (
+    <div className="App">
+      {console.log(investments)}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/financials.js
@@ -0,0 +1,71 @@
+// https://fundresearch.fidelity.com/mutual-funds/composition/316464106 information used for finanical derivatives 
+
+let investments=[{investment_name: 'Fidelity® Value Fund',
+        asset_holdings: {
+            Domestic_Equities: 92.60,
+            International_Equities: 7.32,
+            Bonds: 0,
+            Cash_Other_Assets: 0.08
+        },
+        asset_class: 'Mid-Cap Value',
+        ticker: 'FDVLX'
+    },
+
+    {
+        investment_name: 'Fidelity® Select Biotechnology Portfolio',
+        asset_holdings: {
+            Domestic_Equities: 92.86,
+            International_Equities: 6.49,
+            Bonds: 0,
+            Cash_Other_Assets: 0.65
+        },
+        asset_class: 'Mid-Cap Growth',
+        ticker: 'FBIOX'
+    },
+
+    {investment_name: 'Fidelity® Growth Company Fund',
+        asset_holdings: {
+            Domestic_Equities: 91.03,
+            International_Equities: 8.75,
+            Bonds: 0.05,
+            Cash_Other_Assets: 0.17
+        },
+        asset_class: 'Large Growth',
+        ticker: 'FDGRX'
+    },
+
+    {investment_name: 'Fidelity® Balanced Fund',
+        asset_holdings: {
+            Domestic_Equities: 66.89,
+            International_Equities: 4.11,
+            Bonds: 26.06,
+            Cash_Other_Assets: 2.94
+        },
+        asset_class: 'Large Blend',
+        ticker: 'FBALX'
+    },
+
+    {investment_name: 'Fidelity Freedom® Income Fund',
+        asset_holdings: {
+            Domestic_Equities: 9.01,
+            International_Equities: 12.73,
+            Bonds: 54.45,
+            Cash_Other_Assets: 23.81
+        },
+        asset_class: 'Large Blend',
+        ticker: 'FFFAX'
+    },
+    {investment_name: 'Fidelity® U.S. Bond Index Fund',
+        asset_holdings: {
+            Domestic_Equities: 0,
+            International_Equities: 0,
+            Bonds: 100,
+            Cash_Other_Assets: 0
+        },
+        asset_class: 'High Quality Moderate Sensitivity',
+        ticker: 'FXNAX'
+     },
+];
+
+
+export default investments;
